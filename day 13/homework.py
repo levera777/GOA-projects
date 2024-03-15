5b98bf050e13cbc11f7d4c5b885b158edbb11cd0
levera777 <ilialeverashvili777@gmail.com>
2024-03-15 15:28:19 +0400

    second commit

---
+++ day 13/homework.py
@@ -0,0 +1,10 @@
+budget = float(input("300 "))
+desired_purchase_cost = float(input("230 "))
+
+if budget >= desired_purchase_cost:
+    print("შეუძლებელია ყიდვა")
+else:
+    print("შესაძლებელია ყიდვა")
+
+
+
